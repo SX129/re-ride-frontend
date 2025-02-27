14218f1bac41e5e78937b04237d4c7257e5c2869
Henry Nguyen <nguyenhenry682@gmail.com>
2025-02-27 15:31:41 -0600

    added background video

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Re-Ride",
-  description: "Skip the wait. Ride with Re-Ride",
+  description: "Skip the wait with Re-Ride",
 };
 
 export default function RootLayout({
