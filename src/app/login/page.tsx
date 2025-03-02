438bc9762969ee7e573e7ac81178ae826885a7e9
Henry Nguyen <nguyenhenry682@gmail.com>
2025-03-01 23:06:11 -0600

    added media point for header

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,7 +13,7 @@ export default function LoginPage() {
           src="/videos/intro-video.mp4"
         />
         <div className="glass-effect w-[100%] h-[100%] bg-transparent flex flex-col justify-center items-center">
-          <div className="header-section">
+          <div className="header-section-limited">
             <nav>
               <Link href="/" className="link-style">
                 RE•RIDE
